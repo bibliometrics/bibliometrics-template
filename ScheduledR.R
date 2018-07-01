f070d876c5b564804ba3f18117310ff0b1f8781a
sbalci <drserdarbalci@gmail.com>
2018-07-01 13:08:19 +0300

    updated on 2018-07-01 13:08:15

---
+++ ScheduledR.R
@@ -0,0 +1,19 @@
+library(rmarkdown)
+library(pander)
+library(rstudioapi)
+# 
+Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render(input = "/Users/serdarbalciold/RepTemplates/BibliographicStudies/ScheduleTrialBibliographic.Rmd", output_format = "html_notebook", output_file = "/Users/serdarbalciold/RepTemplates/BibliographicStudies/SchedulePubMedAnalysis.html", quiet = TRUE)
+#
+Sys.sleep(time = 2)
+#
+CommitMessage <- paste("updated on ", Sys.time(), sep = "")
+gitCommand <- paste("cd /Users/serdarbalciold/RepTemplates/BibliographicStudies/ \n git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
+#
+Sys.sleep(time = 2)
+#
+gitTerm <- rstudioapi::terminalCreate(show = FALSE)
+#
+Sys.sleep(time = 2)
+#
+rstudioapi::terminalSend(gitTerm, gitCommand)
+#
