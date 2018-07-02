02813519dcc55ae1b941a34ac011ab3ba5da4a04
sbalci <drserdarbalci@gmail.com>
2018-07-02 19:12:19 +0300

    updated on 2018-07-02 18:56:37

--- ScheduledR.R
+++ ScheduledR.R
@@ -19,9 +19,8 @@ write(b, "~/RepTemplates/BibliographicStudies/b.txt")
 #
 CommitMessage <- paste("updated on ", Sys.time(), sep = "")
 wd <- getwd()
-gitCommand <- paste("git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
+gitCommand <- paste("cd ", wd, " \n git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
 system(command = gitCommand)
-# "cd ", wd, " \n 
 # CommitMessage <- paste("updated on ", Sys.time(), sep = "")
 # gitCommand <- paste("git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
 # cd /Users/serdarbalciold/RepTemplates/BibliographicStudies/ \n
