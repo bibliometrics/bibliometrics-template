005e45cac131aaac25442e8a4945e3625b14206e
sbalci <drserdarbalci@gmail.com>
2018-07-02 17:00:02 +0300

    updated on 2018-07-02 17:00:02

--- ScheduledR.R
+++ ScheduledR.R
@@ -19,15 +19,21 @@ write(b, "~/RepTemplates/BibliographicStudies/b.txt")
 # Sys.sleep(time = 2)
 #
 CommitMessage <- paste("updated on ", Sys.time(), sep = "")
-gitCommand <- paste("git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
+gitCommand <- paste("cd ~/RepTemplates/BibliographicStudies/ \n git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
+#
+system(command = gitCommand, intern = TRUE)
+
+
+# CommitMessage <- paste("updated on ", Sys.time(), sep = "")
+# gitCommand <- paste("git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
 # cd /Users/serdarbalciold/RepTemplates/BibliographicStudies/ \n
 # Sys.sleep(time = 2)
 #
-gitTerm <- rstudioapi::terminalCreate(caption = "gitTerm")
+# gitTerm <- rstudioapi::terminalCreate(caption = "gitTerm")
 # show = FALSE
 # Sys.sleep(time = 2)
 #
-rstudioapi::terminalSend(id = gitTerm, text = gitCommand)
+# rstudioapi::terminalSend(id = gitTerm, text = gitCommand)
 
 c <- c((1 + 2)*33, Sys.time(), "c")
 write(c, "~/RepTemplates/BibliographicStudies/c.txt")
