20f92e7ee2c482a22abd1044661d0c0d6b7a484f
sbalci <drserdarbalci@gmail.com>
2018-07-02 10:53:06 +0300

    updated on 2018-07-02 08:55:31

--- ScheduledR.R
+++ ScheduledR.R
@@ -3,17 +3,23 @@ library(rmarkdown)
 library(rstudioapi)
 # library(cronR)
 
-a <- (1+2)*33
-write(a, "/Users/serdarbalciold/RepTemplates/BibliographicStudies/a.txt")
+a <- c((1 + 2)*33, Sys.time(), "a")
+write(a, "~/RepTemplates/BibliographicStudies/a.txt")
+
+
 
 # 
-# Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render(input = "/Users/serdarbalciold/RepTemplates/BibliographicStudies/ScheduleTrialBibliographic.Rmd", output_format = "html_notebook", output_file = "/Users/serdarbalciold/RepTemplates/BibliographicStudies/index.html", quiet = TRUE)
+Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render(input = "/Users/serdarbalciold/RepTemplates/BibliographicStudies/ScheduleTrialBibliographic.Rmd", output_format = "html_notebook", output_file = "/Users/serdarbalciold/RepTemplates/BibliographicStudies/index.html", quiet = TRUE)
+
+b <- c((1 + 2)*33, Sys.time(), "b")
+write(b, "~/RepTemplates/BibliographicStudies/b.txt")
+
 #
 # Sys.sleep(time = 2)
 #
 CommitMessage <- paste("updated on ", Sys.time(), sep = "")
-gitCommand <- paste("cd /Users/serdarbalciold/RepTemplates/BibliographicStudies/ \n git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
-#
+gitCommand <- paste("git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
+# cd /Users/serdarbalciold/RepTemplates/BibliographicStudies/ \n
 # Sys.sleep(time = 2)
 #
 gitTerm <- rstudioapi::terminalCreate(show = FALSE)
@@ -21,4 +27,6 @@ gitTerm <- rstudioapi::terminalCreate(show = FALSE)
 # Sys.sleep(time = 2)
 #
 rstudioapi::terminalSend(gitTerm, gitCommand)
-#
+
+c <- c((1 + 2)*33, Sys.time(), "c")
+write(c, "~/RepTemplates/BibliographicStudies/c.txt")
